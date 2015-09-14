80e81e3052f4638f4958528b709a960dff25e2a8
jurruh <jurrevriesen@gmail.com>
2015-09-14 22:42:50 +0200

    Changed to WifiHotspotDotNet

--- Exceptions.cs
+++ Exceptions.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WifiHotspotDotnet
+namespace WifiHotspotDotNet
 {
     class PassphraseLengthException : Exception
     {
--- WifiHotspot.cs
+++ WifiHotspot.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WifiHotspotDotnet
+namespace WifiHotspotDotNet
 {
     public class WifiHotspot
     {
